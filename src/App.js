e654a8ba83781c25cffe4a95fccc8875d9e93da6
Deepak Sharma <deepak@softraw.com>
2022-04-10 22:57:47 +0530

    added profile context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import AppRouter from "./components/Routes/AppRouter";
 import AuthContextProvider from "./contexts/AuthContext";
+import ProfileContextProvider from "./contexts/ProfileContext";
 import AttendanceContextProvider from "./contexts/AttendanceContext";
 
 function App() {
   return (
     <AuthContextProvider>
       <AttendanceContextProvider>
-        <AppRouter />
+      <ProfileContextProvider>
+      <AppRouter />
+      </ProfileContextProvider>
       </AttendanceContextProvider>
     </AuthContextProvider>
   );
